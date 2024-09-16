3eb84e6bb44f882565e84edf47dd9940ceed6d33
Jonathan Cano <jonathancanofreta@gmail.com>
2024-09-16 11:53:41 +0200

    Added Suspense to AuthHandler.tsx according to Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,21 @@ import type { Metadata } from "next";
 import Landing from "./components/landing/landing";
 import { AuthHandler } from "./components/authHandler/AuthHandler";
 import "./styles/globals.css";
+import { Suspense } from "react";
 
 export default function IndexPage() {
   return (
     <>
-      <AuthHandler />
+      <Suspense>
+        <AuthHandler />
+      </Suspense>
       <Landing />
     </>
   );
 }
 
 export const metadata: Metadata = {
-  title: 'reddArt', 
+  title: 'reddArt',
   description: 'Your site description here',
   icons: {
     icon: '/favicon.svg',

627bdc5be9f6652789f598acbb56ffef225950fd
Jonathan Cano <jonathancanofreta@gmail.com>
2024-09-25 11:35:29 +0200

    Updated files to allow deployment in Cloudfare

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -38,5 +40,9 @@ const nextConfig = {
     scrollRestoration: true,
   },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;

847597ae02c3b3d10d644852a9bdcf6fb835bbaf
Jonathan Cano <jonathancanofreta@gmail.com>
2024-09-26 14:46:14 +0200

    Updated landing page to add an animation to the Image

--- app/components/landing/landing.tsx
+++ app/components/landing/landing.tsx
@@ -2,25 +2,37 @@
 
 import Image from "next/image"
 import mockup from "@/public/mockup3.jpg"
+import { useInView } from "react-intersection-observer"
 
 import styles from "@/app/styles/landing.module.css"
 
 export default function Landing() {
 
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+    })
+
     return (
         <>
             <div className={`flex bg-light-background h-screen justify-center w-screen`}>
-                <div className="flex ml-56 sm:ml-80 w-full justify-center overflow-hidden p-10">
+                <div className="flex ml-56 sm:ml-80 w-full justify-center overflow-hidden p-8">
+                    <div className={`flex w-full bg-gray-300 rounded-3xl border-8 border-white shadow-xl ${styles.landingBackgroundBlur}`}>
                     <Image
+                        ref={ref}
                         src={mockup}
                         alt="Macbook Pro mockup with landing page"
                         width={550}
                         height={300}
-                        className={`${styles.landingImage} shadow-xl`}
+                        className={`${styles.landingImage} rounded-2xl`}
                         loading="eager"
                         priority={true}
                         placeholder="blur"
+                        style={{
+                            opacity: inView ? 1 : 0,
+                            transition: 'opacity 0.2s cubic-bezier(0.3, 0.2, 0.2, 0.8)'
+                        }}
                     />
+                    </div>
                 </div>
             </div>
         </>

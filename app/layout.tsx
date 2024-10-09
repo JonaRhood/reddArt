7c3f93795d93099447a3e88d1a0d234565096dc7
Jonathan Cano <jonathancanofreta@gmail.com>
2024-10-09 11:36:39 +0200

    Added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 
 import type { ReactNode } from "react";
 import Head from "next/head";
+import { Metadata } from "next";
 import { StoreProvider } from "./StoreProvider";
 import Sidenav from "./components/sidenav/sidenav";
 import "/public/styles/globals.css";
@@ -34,6 +35,20 @@ export default function RootLayout({
     <StoreProvider>
       <html lang="en">
         <Head>
+           {/* Open Graph tags */}
+        <meta property="og:title" content="reddArt" />
+        <meta property="og:description" content="Discover a vibrant collection of curated art galleries from various Reddit communities. Explore stunning artworks, connect with talented artists, and immerse yourself in the creative world of Reddit, all in one place!" />
+        <meta property="og:image" content="https://reddit-client-49g.pages.dev/mockup3.jpg" />
+        <meta property="og:url" content="https://reddit-client-49g.pages.dev/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="reddArt" />
+        <meta name="twitter:description" content="Discover a vibrant collection of curated art galleries from various Reddit communities. Explore stunning artworks, connect with talented artists, and immerse yourself in the creative world of Reddit, all in one place!" />
+        <meta name="twitter:image" content="https://reddit-client-49g.pages.dev/mockup3.jpg" />
+
+          {/* Styles */}
           <link rel="stylesheet" href="/public/styles/global.css" />
           <link rel="stylesheet" href="/public/styles/artReddits.module.css" />
           <link rel="stylesheet" href="/public/styles/artReddits.module.css" />
@@ -75,9 +90,9 @@ export default function RootLayout({
   );
 }
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'reddArt',
-  description: 'Your site description here',
+  description: 'Art Galleries of Reddit',
   icons: {
     icon: '/favicon.svg',
   },

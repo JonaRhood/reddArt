b2d6de9f7bebdfc890f61404dc1a2307daba6182
Jonathan Cano <jonathancanofreta@gmail.com>
2024-10-09 10:06:13 +0200

    updated css for dark skeleton theme for artReddits.tsx

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -21,15 +21,15 @@ export function NavDivSkeleton({ redditAmount }: NavDivSkeletonProps) {
                 <div key={i} 
                 className={`
                     relative flex-column items-center p-2 h-24 border-b-2 overflow-hidden
-                    ${isDarkTheme ? "bg-dark-surface border-gray-800" : "bg-light-surface"}
+                    ${isDarkTheme ? "bg-dark-surface border-gray-900/60" : "bg-light-surface"}
                 `}
                 >
                     <div className="flex items-center">
-                        <Skeleton circle width={50} height={50} baseColor={`${isDarkTheme ? "gray" : "#ebebeb" }`} highlightColor={`${isDarkTheme ? "light-gray" : "#f5f5f5"}`}/>
-                        <Skeleton height={20} width={150} className="ml-3" baseColor={`${isDarkTheme ? "gray" : "#ebebeb" }`} highlightColor={`${isDarkTheme ? "light-gray" : "#f5f5f5"}`} />
+                        <Skeleton circle width={50} height={50} baseColor={`${isDarkTheme ? "#252525" : "#ebebeb" }`} highlightColor={`${isDarkTheme ? "#2f2f2f" : "#f5f5f5"}`}/>
+                        <Skeleton height={20} width={150} className="ml-3" baseColor={`${isDarkTheme ? "#252525" : "#ebebeb" }`} highlightColor={`${isDarkTheme ? "#2f2f2f" : "#f5f5f5"}`} />
                     </div>
                     <div className="flex flex-col justify-center ml-2">
-                        <Skeleton width={35} height={15} className="" baseColor={`${isDarkTheme ? "gray" : "#ebebeb" }`} highlightColor={`${isDarkTheme ? "light-gray" : "#f5f5f5"}`} />
+                        <Skeleton width={35} height={15} className="" baseColor={`${isDarkTheme ? "#252525" : "#ebebeb" }`} highlightColor={`${isDarkTheme ? "#2f2f2f" : "#f5f5f5"}`} />
                     </div>
                     <ChevronRightIcon className="absolute right-4 top-1/2 transform -translate-y-1/2 size-3" />
                 </div>
@@ -37,6 +37,7 @@ export function NavDivSkeleton({ redditAmount }: NavDivSkeletonProps) {
         </>
     )
 }
+
 
 export function GalleryDivSkeleton({ postAmount }: GalleryDivSkeletonProps) {
     return (
